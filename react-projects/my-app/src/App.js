290c70e65a5c61379d705646009ebe57cc81f750
Lancia <31668397+djlancia@users.noreply.github.com>
2025-12-30 18:26:01 -0800

    added !

--- react-projects/my-app/src/App.js
+++ react-projects/my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I did it
+         I did it!
         </p>
         <a
           className="App-link"
